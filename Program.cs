d3a27547ef087e36d50f55a1c7a7e21b09178eb0
CLown1331 <clown1331@gmail.com>
2020-03-26 20:57:25 +0600

    debgu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -12,38 +12,14 @@ namespace Csharp_Contest
     {
         static void Solve()
         {
-            string s = Console.ReadLine();
-            int q = NextInt();
-            int n = s.Length;
-            SegmentTree tree = new SegmentTree(n, s);
-            int ty, l, r, qr;
-            while ((q--) > 0)
+            int n = NextInt();
+            for (int i = 0; i < n; i++) 
             {
-                ty = NextInt();
-                if (ty == 1)
-                {
-                    l = NextInt();
-                    tree.Update(1, 0, n - 1, l - 1, 1 << (NextString()[0] - 'a'));
-                }
-                else
-                {
-                    l = NextInt();
-                    r = NextInt();
-                    qr = tree.Query(1, 0, n - 1, l - 1, r - 1);
-                    Console.WriteLine(BitCount(qr));
-                }
+                int a = NextInt();
+                int b = NextInt();
+                int rem = (a % b == 0) ? 0 : (((a / b) + 1) * b) - a;
+                Console.WriteLine(rem);
             }
-        }
-
-        static int BitCount(int x)
-        {
-            int ret = 0;
-            while (x > 0)
-            {
-                ret += (x & 1);
-                x /= 2;
-            }
-            return ret;
         }
 
         public static void Main(string[] args)
@@ -58,66 +34,6 @@ namespace Csharp_Contest
             Console.Out.Flush();
         }
 
-        class SegmentTree
-        {
-            int[] tree;
-            int nodeCount;
-
-            public SegmentTree(int nodeCount, string s)
-            {
-                tree = new int[nodeCount * 4];
-                this.nodeCount = nodeCount;
-                Build(1, 0, nodeCount - 1, s);
-            }
-
-            private void Build(int node, int b, int e, string s)
-            {
-                if (b == e)
-                {
-                    tree[node] = 1 << (s[b] - 'a');
-                    return;
-                }
-                int left = node << 1;
-                int right = left | 1;
-                int mid = (b + e) >> 1;
-                Build(left, b, mid, s);
-                Build(right, mid + 1, e, s);
-                tree[node] = tree[left] | tree[right];
-            }
-
-            public void Update(int node, int b, int e, int idx, int x)
-            {
-                if (b == e)
-                {
-                    tree[node] = x;
-                    return;
-                }
-                int left = node << 1;
-                int right = left | 1;
-                int mid = (b + e) >> 1;
-                if (idx <= mid) Update(left, b, mid, idx, x);
-                else Update(right, mid + 1, e, idx, x);
-                tree[node] = tree[left] | tree[right];
-            }
-
-            public int Query(int node, int b, int e, int l, int r)
-            {
-                if (r < b || e < l) return 0;
-                if (b >= l && e <= r)
-                {
-                    return tree[node];
-                }
-
-                int left = node << 1;
-                int right = left | 1;
-                int mid = (b + e) >> 1;
-
-                int p1 = Query(left, b, mid, l, r);
-                int p2 = Query(right, mid + 1, e, l, r);
-
-                return p1 | p2;
-            }
-        }
         static int NextInt () => int.Parse(Console_.NextString());
         static long NextLong () => long.Parse(Console_.NextString());
         static double NextDouble () => double.Parse(Console_.NextString());
