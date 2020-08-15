262d379bfc68f17d02edbe86bbe0f755376105a9
CLown1331 <clown1331@yahoo.com>
2020-08-16 00:45:10 +0600

    Update

---
+++ Library/DisjointSet.cs
@@ -0,0 +1,55 @@
+﻿namespace Csharp_Contest.Library
+{
+    class DisjointSet
+    {
+        private int size;
+        private int[] parent;
+        private int[] count;
+
+        private int numberOfComponent;
+
+        public int NumberOfComponent { get { return numberOfComponent; } }
+        public DisjointSet(int size)
+        {
+            this.size = size;
+            this.numberOfComponent = size;
+            this.parent = new int[size];
+            this.count = new int[size];
+            Reset();
+        }
+        public void Reset()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                count[i] = 1;
+                parent[i] = i;
+            }
+            numberOfComponent = size;
+        }
+        public int FindParent(int u)
+        {
+            if (parent[u] == u)
+            {
+                return u;
+            }
+            return parent[u] = FindParent(parent[u]);
+        }
+
+        public bool IsSameSet(int u, int v)
+        {
+            return FindParent(u) == FindParent(v);
+        }
+        public void MergeSet(int u, int v)
+        {
+            if (IsSameSet(u, v))
+            {
+                return;
+            }
+            u = FindParent(u);
+            v = FindParent(v);
+            parent[u] = parent[v];
+            count[v] += count[u];
+            numberOfComponent--;
+        }
+    }
+}
---
+++ Library/FenwickTree.cs
@@ -0,0 +1,70 @@
+﻿namespace Csharp_Contest.Library
+{
+    using System;
+    class FenwickTree<T>
+    {
+        private readonly T[] tree;
+        private readonly int size;
+        private readonly Func<T, T, T> func;
+        private readonly int logn;
+        private Func<T, T, int> comp;
+
+        public FenwickTree(int size, Func<T, T, T> func, Func<T, T, int> comp)
+        {
+            this.size = size;
+            this.tree = new T[size];
+            this.func = func;
+            this.logn = (int)Math.Floor(Math.Log(this.size, 2));
+            this.comp = comp;
+        }
+
+        public void Update(int index, T value)
+        {
+            for (; index < size; index += index & -index)
+            {
+                this.tree[index] = this.func(this.tree[index], value);
+            }
+        }
+
+        public T Query(int index)
+        {
+            T ret = default(T);
+            for (; index > 0; index -= index & -index)
+            {
+                ret = this.func(ret, this.tree[index]);
+            }
+
+            return ret;
+        }
+
+        public int LowerBound(T value)
+        {
+            int l = 1;
+            int r = this.size - 1;
+            while (r - l > 4)
+            {
+                int mid = l + (r - l) / 2;
+                T sum = this.Query(mid);
+                if (this.comp(sum, value) < 0)
+                {
+                    l = mid;
+                }
+                else
+                {
+                    r = mid;
+                }
+            }
+
+            for (; l <= r; l++)
+            {
+                T sum = this.Query(l);
+                if (this.comp(sum, value) == 0)
+                {
+                    return l;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
