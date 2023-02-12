946867bbd9cb25ffee79e98285c0fa174fc1b3ab
Araf Al Jami <clown1331@gmail.com>
2023-02-12 19:10:32 +0000

    Update validator

--- Csharp-Contest/Validator.cs
+++ Csharp-Contest/Validator.cs
@@ -1,6 +1,6 @@
 // Validator.cs
 // Author: Araf Al Jami
-// Last Updated: 12-02-2023 18:12
+// Last Updated: 12-02-2023 19:02
 
 namespace CLown1331
 {
@@ -14,30 +14,42 @@ namespace CLown1331
     {
         public static void Process()
         {
-            var testCasesCntFile = new StreamReader("./test_cases/cnt");
-            int testCases = int.Parse(testCasesCntFile.ReadLine() ?? string.Empty);
-            var stopWatch = new System.Diagnostics.Stopwatch();
-            long totalTime = stopWatch.ElapsedMilliseconds;
-            stopWatch.Start();
-            var passedCount = 0;
-            string outputFilePath = Path.Combine(Utils.GetRootDirectoryPath(), "output.txt");
-            File.WriteAllText(outputFilePath, string.Empty);
-            for (var testCase = 1; testCase <= testCases; testCase++)
+            if (!TaskSolution.ValidateTestCases)
             {
-                var solution = new TaskSolution();
-                string inputFileNameForTestCase = "./test_cases/" + testCase + ".in";
-                string outputFileNameForTestCase = "./test_cases/" + testCase + ".out";
-                string validFileNameForTestCase = "./test_cases/" + testCase + ".val";
-                Reader.SetInputReader(new StreamReader(inputFileNameForTestCase));
-                FileStream outputFileForTestCase = File.Create(outputFileNameForTestCase);
-                var outputStream = new MultiStream(outputFileForTestCase, Console.OpenStandardOutput());
+                var outputStream = new MultiStream(
+                    File.Create(Path.Combine(Utils.GetRootDirectoryPath(), "output.txt")),
+                    Console.OpenStandardOutput());
                 Output.OutputPrinter = new Printer(outputStream);
-                stopWatch.Restart();
-                solution.Solve();
-                outputFileForTestCase.Close();
-                File.WriteAllText(outputFilePath, File.ReadAllText(outputFilePath) + File.ReadAllText(outputFileNameForTestCase));
-                if (TaskSolution.ValidateTestCases)
+                for (var testCase = 1; !Reader.IsEndOfStream; testCase++)
                 {
+                    Output.ErrorPrinter.WriteLine("---- Case {0} ----", testCase);
+                    new TaskSolution().Solve();
+                    Output.ErrorPrinter.WriteLine("---- Case {0} ----", testCase);
+                }
+            }
+            else
+            {
+                var testCasesCntFile = new StreamReader("./test_cases/cnt");
+                int testCases = int.Parse(testCasesCntFile.ReadLine() ?? string.Empty);
+                var stopWatch = new System.Diagnostics.Stopwatch();
+                long totalTime = stopWatch.ElapsedMilliseconds;
+                stopWatch.Start();
+                var passedCount = 0;
+                string outputFilePath = Path.Combine(Utils.GetRootDirectoryPath(), "output.txt");
+                File.WriteAllText(outputFilePath, string.Empty);
+                for (var testCase = 1; testCase <= testCases; testCase++)
+                {
+                    string inputFileNameForTestCase = "./test_cases/" + testCase + ".in";
+                    string outputFileNameForTestCase = "./test_cases/" + testCase + ".out";
+                    string validFileNameForTestCase = "./test_cases/" + testCase + ".val";
+                    Reader.SetInputReader(new StreamReader(inputFileNameForTestCase));
+                    FileStream outputFileForTestCase = File.Create(outputFileNameForTestCase);
+                    var outputStream = new MultiStream(outputFileForTestCase, Console.OpenStandardOutput());
+                    Output.OutputPrinter = new Printer(outputStream);
+                    stopWatch.Restart();
+                    new TaskSolution().Solve();
+                    outputFileForTestCase.Close();
+                    File.WriteAllText(outputFilePath, File.ReadAllText(outputFilePath) + File.ReadAllText(outputFileNameForTestCase));
                     bool success = File.ReadAllText(outputFileNameForTestCase) == File.ReadAllText(validFileNameForTestCase);
                     if (success)
                     {
@@ -52,22 +64,19 @@ namespace CLown1331
                     }
 
                     Console.ForegroundColor = ConsoleColor.Black;
+                    stopWatch.Stop();
+                    totalTime += stopWatch.ElapsedMilliseconds;
                 }
 
-                stopWatch.Stop();
-                totalTime += stopWatch.ElapsedMilliseconds;
-            }
-
-            if (TaskSolution.ValidateTestCases)
-            {
                 Console.ForegroundColor = passedCount == testCases ? ConsoleColor.DarkGreen : ConsoleColor.DarkRed;
 
                 Output.ErrorPrinter.WriteLine("{0}/{1} Tests passed!", passedCount, testCases);
                 Console.ForegroundColor = ConsoleColor.Black;
+
+                Output.ErrorPrinter.WriteLine($"Runtime: {totalTime}ms");
+                stopWatch.Stop();
             }
 
-            Output.ErrorPrinter.WriteLine($"Runtime: {totalTime}ms");
-            stopWatch.Restart();
             Utils.CreateFileForSubmission(Output.ErrorPrinter);
             if (System.Diagnostics.Debugger.IsAttached)
             {
